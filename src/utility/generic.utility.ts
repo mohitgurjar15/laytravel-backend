84b738b8919b48e096d370685908cecc202364cc
suresh555 <suresh@itoneclick.com>
2020-10-23 10:59:24 +0530

    changes

--- src/utility/generic.utility.ts
+++ src/utility/generic.utility.ts
@@ -46,8 +46,6 @@ export class  Generic{
     }
     
     static formatPriceDecimal(price){
-
-        console.log(typeof price,price,"--------------")
         return Number(price.toFixed(2))
     }
 }
