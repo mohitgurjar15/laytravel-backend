b72dfd67cc3e461e24eed9680edf8f2d35bc93a7
parthvi67 <viraniparth00001@gmail.com>
2020-12-04 20:42:21 +0530

    payment reminder

---
+++ src/config/email_template/payment-reminder.html.ts
@@ -0,0 +1,51 @@
+import { EmailHeader } from "./header.html";
+import { EmailFooter } from "./footer.html";
+import { BaseUrl } from "../base-url";
+
+export function PaymentReminderMail(param: { userName: string, amount: string, date: string }) {
+    const content = `<!-- header Text section start -->
+<table width="100%" border="0" cellspacing="0" cellpadding="0"  style="background: #f2f2f2;" class="full-wrap">
+    <tr>
+        <td align="center" valign="top">
+            <table align="center" style="width:600px; max-width:600px; table-layout:fixed;" class="oc_wrapper" width="600" border="0" cellspacing="0" cellpadding="0">
+                <tr>
+                    <td align="center" valine="top" style="background-color: #ffffff;">
+                        <table width="600" border="0" cellspacing="0" cellpadding="0" align="center" style="width: 600px;" class="oc_wrapper">
+                            <tbody>
+                                <tr>
+                                    <td align="center" valine="top" style="padding: 10px 15px 15px; background: #ffffff;">
+                                        <table width="100%" border="0" cellspacing="0" cellpadding="0" align="center" style="width: 100%">
+                                            <tbody>
+                                                <tr>
+                                                    <td align="left" valign="top" style="font-family: 'Poppins', sans-serif;font-size: 22px; line-height: 24px; color: #444; font-weight:700; padding-top: 15px; text-align: center;">Partial Payment Reminder</td>
+                                                </tr>
+                                                <tr>
+                                                    <td align="left" valign="top" style="font-family: 'Poppins', sans-serif;font-size: 14px; line-height: 18px; color: #000;padding-top: 15px; text-align: center;">Hi ${param.userName},</td>
+                                                </tr>
+                                                <tr>
+                                                    <td align="left" valign="top" style="font-family: 'Poppins', sans-serif;font-size: 14px; line-height: 18px; color: #000;padding-top: 15px; text-align: center;">Just a friendly reminder that your instalment amount of ${param.amount} will be collected on ${param.date} Please ensure you have sufficient funds on your account and all the banking information is up-to-date.</td>
+                                                </tr>
+                                                <tr>
+                                                    <td align="left" valign="top" style="font-family: 'Poppins', sans-serif;font-size: 14px; line-height: 18px; color: #000;padding-top: 15px; text-align: center;">We’re as excited for your trip as you are and want to ensure that every payment towards your trip goes through as smoothly as possible. However, if you’d like to turn off these payment notifications, please go to your notification settings to do so.</td>
+                                                </tr>
+                                                <tr>
+                                                    <td align="left" valign="top" style="font-family: 'Poppins', sans-serif;font-size: 14px; line-height: 18px; color: #000;padding-top: 15px; text-align: center;">Enjoy!</td>
+                                                </tr>
+                                                <tr>
+                                                    <td align="left" valign="top" style="font-family: 'Poppins', sans-serif;font-size: 14px; line-height: 18px; color: #000;padding-top: 15px; text-align: center;">The Laytrip Team</td>
+                                                </tr>													
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
+<!-- header text section End -->`;
+    return EmailHeader + content + EmailFooter;
+}
