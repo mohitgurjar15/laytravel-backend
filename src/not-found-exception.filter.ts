b20dd3c504460a9009f3c19ae93ed9d838f87cbe
parthvi67 <viraniparth00001@gmail.com>
2020-12-23 15:19:16 +0530

    multilanguge added

--- src/not-found-exception.filter.ts
+++ src/not-found-exception.filter.ts
@@ -1,9 +1,10 @@
 import { NotFoundException, Catch, ExceptionFilter, ArgumentsHost } from "@nestjs/common";
 import {  Response } from "express";
+import { Translation } from "./utility/translation.utility";
 
 @Catch(NotFoundException)
 export class NotFoundExceptionFilter implements ExceptionFilter {
-	catch(exception: NotFoundException, host: ArgumentsHost) {
+	async catch(exception: NotFoundException, host: ArgumentsHost) {
 		const ctx = host.switchToHttp();
 		const response = ctx.getResponse<Response>();
 		const errors = this.filterResponse(exception.getResponse()["message"]);
@@ -11,7 +12,7 @@ export class NotFoundExceptionFilter implements ExceptionFilter {
 			.status(404)
 			// you can manipulate the response here
 			.json({
-				message: errors[0].display_error,
+				message: await Translation.Translater('ES', 'responce', errors[0].display_error),
 				developer_errors: errors,
 			});
 	}
