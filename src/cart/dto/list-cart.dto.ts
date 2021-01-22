09b0607a3f88dfa4215214d1cd9f6bdb9e891860
Suresh Suthar <suresh@itoneclick.com>
2021-01-22 11:02:22 +0530

    cart list parrellal flight api call

---
+++ src/cart/dto/list-cart.dto.ts
@@ -0,0 +1,15 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsArray, IsEnum, IsNotEmpty, IsOptional, ValidateIf, ValidateNested, ValidationArguments } from 'class-validator';
+import { errorMessage } from 'src/config/common.config';
+import { InstalmentType } from 'src/enum/instalment-type.enum';
+import { ModulesName } from 'src/enum/module.enum';
+import { PaymentType } from 'src/enum/payment-type.enum';
+export class ListCartDto {
+
+	@ApiPropertyOptional({
+		description: "Enter live availiblity",
+		example: true
+	})
+	live_availiblity: boolean;
+}
