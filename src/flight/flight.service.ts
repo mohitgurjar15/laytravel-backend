b9548696ab2ac043e40105a1bb7dc180f100d6ed
Suresh Suthar <suresh@itoneclick.com>
2021-01-24 11:48:01 +0530

    temprary changes for roundtripzip serch api

--- src/flight/flight.service.ts
+++ src/flight/flight.service.ts
@@ -309,8 +309,8 @@ export class FlightService {
 		}
 		const currencyDetails = await Generic.getAmountTocurrency(headers.currency);
 		const result = new Promise((resolve) =>
-			//resolve(mystifly.roundTripSearchZip(searchFlightDto, user))
-			resolve(mystifly.roundTripSearchZip(searchFlightDto, user, mystiflyConfig, sessionToken, module, currencyDetails))
+			resolve(mystifly.roundTripSearch(searchFlightDto, user))
+			//resolve(mystifly.roundTripSearchZip(searchFlightDto, user, mystiflyConfig, sessionToken, module, currencyDetails))
 
 		);
 		return result;
