f30209ebbfed4cb435663cb4f321bf63c320fda7
Suresh Suthar <suresh@itoneclick.com>
2021-02-15 15:25:03 +0530

    changes added

--- src/cart/dto/add-in-cart.dto.ts
+++ src/cart/dto/add-in-cart.dto.ts
@@ -107,9 +107,9 @@ export class AddInCartDto {
 	})
 	number_and_children_ages: Array<Number>;
 
-	@ApiProperty({
+	@ApiPropertyOptional({
 		description: "enter guest id ",
 		example: "25f56893-3759-46f1-a845-b90c2c3c488a"
 	})
-	guestId: string;
+	guest_id: string;
 }
