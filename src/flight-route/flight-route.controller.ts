895c669319c790129c344de10f7175c2a0becfc3
parthvirani <parthvirani@itoneclick.com>
2021-03-23 14:22:03 +0530

    flight route api added

--- src/flight-route/flight-route.controller.ts
+++ src/flight-route/flight-route.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, HttpCode, Post, Query, UseGuards } from "@nestjs/common";
+import { BadRequestException, Body, Controller, Delete, Get, HttpCode, NotFoundException, Param, Patch, Post, Put, Query, Req, UploadedFiles, UseGuards, UseInterceptors } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 import {
     ApiBearerAuth,
+    ApiConsumes,
     ApiOperation,
     ApiResponse,
     ApiTags,
@@ -12,8 +13,16 @@ import { RolesGuard } from "src/guards/role.guard";
 import { FlightRouteService } from "./flight-route.service";
 import { ListFlightRouteDto } from "./dto/list-flight-route.dto";
 import { AddFlightRouteDto } from "./dto/add-flight-route.dto";
+import { diskStorage } from "multer";
 import { User } from "src/entity/user.entity";
 import { GetUser } from "src/auth/get-user.dacorator";
+import { UpdateFlightRouteDto } from "./dto/update-flight-route.dto";
+import {EnableDisableFlightRouteDto} from "./dto/enable-disable-route.dto"
+import { FileFieldsInterceptor } from "@nestjs/platform-express";
+import { ImportRouteDto } from "./dto/import-route.dto";
+import { csvFileDto } from "src/user/dto/csv-file.dto";
+import { SiteUrl } from "src/decorator/site-url.decorator";
+import { csvFileFilter, editFileName } from "src/auth/file-validator";
 @ApiTags("Flight Route")
 @Controller("flight-route")
 @ApiBearerAuth()
@@ -39,7 +48,7 @@ export class FlightRouteController {
         );
     }
 
-    @Post("create")
+    @Post()
     @Roles(Role.SUPER_ADMIN, Role.ADMIN, Role.SUPPORT)
     @ApiBearerAuth()
     @UseGuards(AuthGuard(), RolesGuard)
@@ -55,6 +64,130 @@ export class FlightRouteController {
         @Body() addFlightRouteDto: AddFlightRouteDto,
         @GetUser() user: User
     ) {
-        return await this.flightRouteService.addFlightRoute(addFlightRouteDto, user);
+        return await this.flightRouteService.addFlightRoute(
+            addFlightRouteDto,
+            user
+        );
     }
+
+    @Put(":id")
+    @Roles(Role.SUPER_ADMIN, Role.ADMIN, Role.SUPPORT)
+    @ApiBearerAuth()
+    @UseGuards(AuthGuard(), RolesGuard)
+    @ApiOperation({ summary: "Add new flight route" })
+    @ApiResponse({ status: 200, description: "Api success" })
+    @ApiResponse({
+        status: 422,
+        description: "Bad Request or API error message",
+    })
+    @ApiResponse({ status: 500, description: "Internal server error!" })
+    @HttpCode(200)
+    async updateRoute(
+        @Body() updateFlightRouteDto: UpdateFlightRouteDto,
+        @GetUser() user: User,
+        @Param("id") id: number
+    ) {
+        return await this.flightRouteService.updateFlightRoute(
+            id,
+            updateFlightRouteDto,
+            user
+        );
+    }
+
+    @Patch("active-deactive/:id")
+    @Roles(Role.SUPER_ADMIN, Role.ADMIN, Role.SUPPORT)
+    @ApiBearerAuth()
+    @UseGuards(AuthGuard(), RolesGuard)
+    @ApiOperation({ summary: "Add new flight route" })
+    @ApiResponse({ status: 200, description: "Api success" })
+    @ApiResponse({
+        status: 422,
+        description: "Bad Request or API error message",
+    })
+    @ApiResponse({ status: 500, description: "Internal server error!" })
+    @HttpCode(200)
+    async activeDeactiveRoute(
+        @Body() enableDisableFlightRouteDto: EnableDisableFlightRouteDto,
+        @GetUser() user: User,
+        @Param("id") id: number
+    ) {
+        return await this.flightRouteService.enableDisableFlightRoute(
+            id,
+            enableDisableFlightRouteDto,
+            user
+        );
+    }
+
+    @Delete(":id")
+    @Roles(Role.SUPER_ADMIN, Role.ADMIN, Role.SUPPORT)
+    @ApiBearerAuth()
+    @UseGuards(AuthGuard(), RolesGuard)
+    @ApiOperation({ summary: "Add new flight route" })
+    @ApiResponse({ status: 200, description: "Api success" })
+    @ApiResponse({
+        status: 422,
+        description: "Bad Request or API error message",
+    })
+    @ApiResponse({ status: 500, description: "Internal server error!" })
+    @HttpCode(200)
+    async deleteRoute(
+       @GetUser() user: User,
+        @Param("id") id: number
+    ) {
+        return await this.flightRouteService.deleteFlightRoute(
+            id,
+            user
+        );
+    }
+
+
+    @Post("report/import")
+	@ApiConsumes("multipart/form-data")
+	@ApiBearerAuth()
+	@UseGuards(AuthGuard(), RolesGuard)
+	@Roles(Role.SUPER_ADMIN, Role.ADMIN, Role.SUPPORT)
+	@ApiOperation({ summary: "import flight route" })
+	@ApiResponse({ status: 200, description: "Api success" })
+	@ApiResponse({ status: 422, description: "Bad Request or API error message" })
+	@ApiResponse({
+		status: 403,
+		description: "You are not allowed to access this resource.",
+	})
+	@ApiResponse({ status: 404, description: "User not found!" })
+	@ApiResponse({ status: 500, description: "Internal server error!" })
+	@UseInterceptors(
+		FileFieldsInterceptor(
+			[
+				{ name: "file", maxCount: 1 }
+			],
+			{
+				storage: diskStorage({
+					destination: "./assets/otherfiles",
+					filename: editFileName,
+				}),
+				fileFilter: csvFileFilter
+			},
+		),
+	)
+	@HttpCode(200)
+	async importRoute(
+		@Body() importUserDto: ImportRouteDto,
+		@UploadedFiles() files: csvFileDto,
+		@Req() req,
+		@GetUser() user: User,
+		@SiteUrl() siteUrl: string,
+	) {
+		if (req.fileValidationError) {
+			throw new BadRequestException(`${req.fileValidationError}`);
+		}
+		if (typeof files.file[0] == "undefined") {
+			throw new NotFoundException(`file is not available&&&file`);
+		}
+		const userId = user.userId;
+		const file = files.file;
+
+		return await this.flightRouteService.importFlightRoute(importUserDto, file, userId, siteUrl)
+	}
 }
+
+
--- src/flight-route/flight-route.service.ts
+++ src/flight-route/flight-route.service.ts
@@ -1,18 +1,26 @@
-import { BadRequestException, ConflictException, Injectable, NotFoundException } from "@nestjs/common";
+import {
+    BadRequestException,
+    ConflictException,
+    Injectable,
+    NotFoundException,
+} from "@nestjs/common";
 import { FlightRoute } from "src/entity/flight-route.entity";
 import { LaytripCategory } from "src/entity/laytrip-category.entity";
 import { User } from "src/entity/user.entity";
 import { airports } from "src/flight/airports";
+import { Activity } from "src/utility/activity.utility";
 import { getConnection } from "typeorm";
 import { AddFlightRouteDto } from "./dto/add-flight-route.dto";
 import { EnableDisableFlightRouteDto } from "./dto/enable-disable-route.dto";
+import { ImportRouteDto } from "./dto/import-route.dto";
 import { ListFlightRouteDto } from "./dto/list-flight-route.dto";
+import { UpdateFlightRouteDto } from "./dto/update-flight-route.dto";
 
 @Injectable()
 export class FlightRouteService {
     async listFlightRoutes(listFlightRouteDto: ListFlightRouteDto) {
         const { limit, page_no, search, status } = listFlightRouteDto;
-        let where = `1=1`;
+        let where = `("route"."is_deleted" = false)`;
 
         const take = limit || 10;
         const skip = (page_no - 1) * limit || 0;
@@ -33,6 +41,7 @@ export class FlightRouteService {
         }
         let [result, count] = await getConnection()
             .createQueryBuilder(FlightRoute, "route")
+            .leftJoinAndSelect("route.category", "category")
             .where(where)
             .skip(skip)
             .take(take)
@@ -61,18 +70,6 @@ export class FlightRouteService {
 
         if (!category) {
             throw new BadRequestException("Given category id not found.");
-        }
-
-        let where = ` 
-            "route"."to_airport_code" = '${to_airport_codes}' AND
-            "route"."from_airport_code" = '${from_airport_codes}'`;
-
-        const dublicate = await getConnection()
-            .createQueryBuilder(FlightRoute, "route")
-            .where(where)
-            .getOne();
-        if (dublicate) {
-            throw new ConflictException("Given route already added.");
         }
 
         let parentFromCode = "";
@@ -112,25 +109,44 @@ export class FlightRouteService {
             );
         }
 
+        let dublicateRoutes = [];
+
         let parentRoute: FlightRoute;
         if (parentFromCode && parentToCode && parentToCode != parentToCode) {
-            const fromAirport = airports[parentFromCode];
-            const toAirport = airports[parentToCode];
-            const route = new FlightRoute();
-            route.categoryId = category.id;
-            route.createBy = user.userId;
-            route.fromAirportCity = fromAirport.city;
-            route.fromAirportCode = fromAirport.code;
-            route.fromAirportCountry = fromAirport.country;
-            route.fromAirportName = fromAirport.name;
-            route.toAirportCity = toAirport.city;
-            route.toAirportCode = toAirport.code;
-            route.toAirportCountry = toAirport.country;
-            route.toAirportName = toAirport.name;
-            route.status = true;
-            route.isDeleted = false;
-            route.createDate = new Date();
-            parentRoute = await route.save();
+            let where = ` "route"."is_deleted" = false AND
+            "route"."to_airport_code" = '${parentToCode}' AND
+            "route"."from_airport_code" = '${parentFromCode}'`;
+
+            const dublicate = await getConnection()
+                .createQueryBuilder(FlightRoute, "route")
+                .where(where)
+                .getOne();
+            if (dublicate) {
+                let r = {
+                    fromCode: parentFromCode,
+                    ToCode: parentToCode,
+                };
+                dublicateRoutes.push(r);
+                //throw new ConflictException("Given route already added.");
+            } else {
+                const fromAirport = airports[parentFromCode];
+                const toAirport = airports[parentToCode];
+                const route = new FlightRoute();
+                route.categoryId = category.id;
+                route.createBy = user.userId;
+                route.fromAirportCity = fromAirport.city;
+                route.fromAirportCode = fromAirport.code;
+                route.fromAirportCountry = fromAirport.country;
+                route.fromAirportName = fromAirport.name;
+                route.toAirportCity = toAirport.city;
+                route.toAirportCode = toAirport.code;
+                route.toAirportCountry = toAirport.country;
+                route.toAirportName = toAirport.name;
+                route.status = true;
+                route.isDeleted = false;
+                route.createDate = new Date();
+                parentRoute = await route.save();
+            }
         }
 
         for await (const fromCode of from_airport_codes) {
@@ -141,53 +157,245 @@ export class FlightRouteService {
                     parentFromCode != fromCode.airport_code ||
                     parentToCode != toCode.airport_code
                 ) {
-                    const route = new FlightRoute();
-                    route.categoryId = category.id;
-                    route.createBy = user.userId;
-                    route.parentRoute = parentRoute || null;
-                    route.fromAirportCity = fromAirport.city;
-                    route.fromAirportCode = fromAirport.code;
-                    route.fromAirportCountry = fromAirport.country;
-                    route.fromAirportName = fromAirport.name;
-                    route.toAirportCity = toAirport.city;
-                    route.toAirportCode = toAirport.code;
-                    route.toAirportCountry = toAirport.country;
-                    route.toAirportName = toAirport.name;
-                    route.status = true;
-                    route.createDate = new Date();
-                    route.isDeleted = false;
-                    await route.save();
+                    let where = ` "route"."is_deleted" = false AND
+                            "route"."to_airport_code" = '${toAirport.code}' AND
+                            "route"."from_airport_code" = '${fromAirport.code}'`;
+
+                    const dublicate = await getConnection()
+                        .createQueryBuilder(FlightRoute, "route")
+                        .where(where)
+                        .getOne();
+                    if (dublicate) {
+                        let r = {
+                            fromCode: fromAirport.code,
+                            ToCode: toAirport.code,
+                        };
+                        dublicateRoutes.push(r);
+                        //throw new ConflictException(
+                        //  "Given route already added."
+                        //);
+                    } else {
+                        const route = new FlightRoute();
+                        route.categoryId = category.id;
+                        route.createBy = user.userId;
+                        route.parentRoute = parentRoute || null;
+                        route.fromAirportCity = fromAirport.city;
+                        route.fromAirportCode = fromAirport.code;
+                        route.fromAirportCountry = fromAirport.country;
+                        route.fromAirportName = fromAirport.name;
+                        route.toAirportCity = toAirport.city;
+                        route.toAirportCode = toAirport.code;
+                        route.toAirportCountry = toAirport.country;
+                        route.toAirportName = toAirport.name;
+                        route.status = true;
+                        route.createDate = new Date();
+                        route.isDeleted = false;
+                        await route.save();
+                    }
                 }
             }
         }
 
+        Activity.logActivity(
+            user.userId,
+            "flight-route",
+            `Your routes added in ${category.name} category`
+        );
+        
         return {
             message: `Your routes added in ${category.name} category`,
+            dublicateRoutes,
         };
     }
 
     async enableDisableFlightRoute(
         id: number,
-        enableDisableFlightRouteDto : EnableDisableFlightRouteDto,
-        user : User
+        enableDisableFlightRouteDto: EnableDisableFlightRouteDto,
+        user: User
     ) {
-        const {status} = enableDisableFlightRouteDto
+        const { status } = enableDisableFlightRouteDto;
         const route = await getConnection()
             .createQueryBuilder(FlightRoute, "route")
-            .where(`id = ${id} `)
+            .where(`"id" = ${id} AND "is_deleted" = false`)
             .getOne();
-        if (route) {
+        if (!route) {
+            throw new NotFoundException("Given route not found.");
+        }
+        const previous = JSON.stringify(route);
+        route.updateDate = new Date();
+        route.updateBy = user.userId;
+        route.status = status;
+
+        const current = await route.save();
+        Activity.logActivity(
+            user.userId,
+            "flight-route",
+            `Flight route status changed successfully`,
+            previous,
+            JSON.stringify(current)
+        );
+        return {
+            message: `Flight route status changed successfully`,
+        };
+    }
+
+    async deleteFlightRoute(id: number, user: User) {
+        const route = await getConnection()
+            .createQueryBuilder(FlightRoute, "route")
+            .where(`"id" = ${id} AND "is_deleted" = false`)
+            .getOne();
+        if (!route) {
+            throw new ConflictException("Given route not found.");
+        }
+        const previous = JSON.stringify(route);
+        route.updateDate = new Date();
+        route.updateBy = user.userId;
+        route.isDeleted = true;
+
+        const current = await route.save();
+        Activity.logActivity(
+            user.userId,
+            "flight-route",
+            `Flight route deleted `,
+            previous,
+            JSON.stringify(current)
+        );
+        return {
+            message: `Flight route deleted successfully`,
+        };
+    }
+
+    async updateFlightRoute(
+        id: number,
+        updateFlightRouteDto: UpdateFlightRouteDto,
+        user: User
+    ) {
+        const { category_id } = updateFlightRouteDto;
+        const route = await getConnection()
+            .createQueryBuilder(FlightRoute, "route")
+            .where(`"id" = ${id} AND "is_deleted" = false`)
+            .getOne();
+        if (!route) {
             throw new ConflictException("Given route not found.");
         }
 
-        route.updateDate = new Date()
-        route.updateBy = user.userId
-        route.status = status
+        const category = await getConnection()
+            .createQueryBuilder(LaytripCategory, "category")
+            .where(`"id" =:id `, { id: category_id })
+            .getOne();
 
-        await route.save()
-
-        return {
-            message : `Flight route status changed successfully`
+        if (!category) {
+            throw new BadRequestException("Given category id not found.");
         }
+        const previous = JSON.stringify(route);
+        route.updateDate = new Date();
+        route.updateBy = user.userId;
+        route.categoryId = category_id;
+
+        const current = await route.save();
+        Activity.logActivity(
+            user.userId,
+            "flight-route",
+            `Flight route deleted `,
+            previous,
+            JSON.stringify(current)
+        );
+        return {
+            message: `Flight route updated successfully`,
+        };
+    }
+
+    async importFlightRoute(
+        importRouteDto: ImportRouteDto,
+        file,
+        userId: string,
+        siteUrl: string
+    ) {
+        var count = 0;
+        const unsuccessRecord = new Array();
+        const csvData = [];
+        const csv = require("csvtojson");
+        const array = await csv().fromFile("./" + file[0].path);
+
+        let errors = [];
+        let dublicateRoutes = []
+
+        for (let index = 0; index < array.length; index++) {
+            var row = array[index];
+            if (row) {
+                let categoryId = parseInt(row.category_id);
+                if (
+                    typeof airports[row.from_airport_code] != "undefined" &&
+                    typeof airports[row.to_airport_code] != "undefined" &&
+                    typeof categoryId == "number"
+                ) {
+                    const category = await getConnection()
+                        .createQueryBuilder(LaytripCategory, "category")
+                        .where(`"id" =:id `, { id: row.category_id })
+                        .getOne();
+
+                    if (!category) {
+                        errors.push(
+                            `Wrong category id for route ${row.from_airport_code} to ${row.to_airport_code}`
+                        );
+                    }
+
+                    let where = ` "route"."is_deleted" = false AND
+                        "route"."to_airport_code" = '${row.to_airport_code}' AND
+                        "route"."from_airport_code" = '${row.from_airport_code}'`;
+
+                    const dublicate = await getConnection()
+                        .createQueryBuilder(FlightRoute, "route")
+                        .where(where)
+                        .getOne();
+                    if (dublicate) {
+                        let r = {
+                            fromCode: row.from_airport_code,
+                            ToCode: row.to_airport_code,
+                        };
+                        dublicateRoutes.push(r);
+                        //throw new ConflictException("Given route already added.");
+                    } else {
+                        const fromAirport = airports[row.from_airport_code];
+                        const toAirport = airports[row.to_airport_code];
+                        const route = new FlightRoute();
+                        route.categoryId = category.id;
+                        route.createBy = userId;
+                        route.parentRoute = null;
+                        route.fromAirportCity = fromAirport.city;
+                        route.fromAirportCode = fromAirport.code;
+                        route.fromAirportCountry = fromAirport.country;
+                        route.fromAirportName = fromAirport.name;
+                        route.toAirportCity = toAirport.city;
+                        route.toAirportCode = toAirport.code;
+                        route.toAirportCountry = toAirport.country;
+                        route.toAirportName = toAirport.name;
+                        route.status = true;
+                        route.createDate = new Date();
+                        route.isDeleted = false;
+                        await route.save();
+                        count++;
+                    }
+                } else {
+                    var error_message = "";
+                    if (typeof airports[row.from_airport_code] == "undefined") {
+                        error_message += `From Airport code ${row.from_airport_code} not found.||`;
+                    }
+                    if (typeof airports[row.to_airport_code] == "undefined") {
+                        error_message += `To Airport code ${row.from_airport_code} not found.||`;
+                    }
+                    if (typeof row.category_id != "number") {
+                        error_message += `Wrong category id for route ${row.from_airport_code} to ${row.to_airport_code}.||`;
+                    }
+                    errors.push(error_message);
+                }
+            }
+        }
+        Activity.logActivity(
+            userId,
+            "flight-route",
+            `Import flight route`
+        );
+        return { importCount: count, unsuccessRecord: errors, dublicateRoutes };
     }
 }
