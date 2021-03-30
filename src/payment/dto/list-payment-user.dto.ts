46d05a9a0c79e13f22bf9882750f6c8c7e1713bf
parthvirani <parthvirani@itoneclick.com>
2021-03-30 18:15:43 +0530

    changes added

--- src/payment/dto/list-payment-user.dto.ts
+++ src/payment/dto/list-payment-user.dto.ts
@@ -1,60 +1,64 @@
 import { IsNotEmpty } from "class-validator";
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
 import { errorMessage } from "src/config/common.config";
 export class ListPaymentUserDto {
-
     @IsNotEmpty({
-        message : `Please enter limit&&&limit&&&${errorMessage}`
+        message: `Please enter limit&&&limit&&&${errorMessage}`,
     })
     @ApiProperty({
-        description:'Limit',
-        example:10
+        description: "Limit",
+        example: 10,
     })
-    limit:number;
+    limit: number;
 
     @IsNotEmpty({
-        message : `Please enter page number&&&page&&&${errorMessage}`
+        message: `Please enter page number&&&page&&&${errorMessage}`,
     })
     @ApiProperty({
-        description:'Page number',
-        example:1
+        description: "Page number",
+        example: 1,
     })
-    page_no:number;
+    page_no: number;
 
     @ApiPropertyOptional({
-        description:'search from start date',
-        example:""
+        description: "search from start date",
+        example: "",
     })
     start_date: Date;
 
     @ApiPropertyOptional({
-        description:'search to end date',
-        example:""
+        description: "search to end date",
+        example: "",
     })
     end_date: Date;
 
     @ApiPropertyOptional({
-        description:'Payment module type',
-        example:''
+        description: "Payment module type",
+        example: "",
     })
     module_id: number;
 
     @ApiPropertyOptional({
-        description:'instalment status',
-        example:''
+        description: "instalment status",
+        example: "",
     })
     status: number;
 
     @ApiPropertyOptional({
-        description:'instalment type',
-        example:""
+        description: "instalment type",
+        example: "",
     })
-    instalment_type:string;
+    instalment_type: string;
 
     @ApiPropertyOptional({
-        description:'Booking id',
-        example:""
+        description: "Booking id",
+        example: "",
     })
-    booking_id:string;
+    booking_id: string;
 
+    @ApiPropertyOptional({
+        description: "product id",
+        example: "",
+    })
+    product_id: string;
 }
