c653eb707fee49acbfc0836d733add616c54ba0b
parthvirani <parthvirani@itoneclick.com>
2021-04-01 17:42:07 +0530

    changes addded

--- src/deal/dto/add-deal.dto.ts
+++ src/deal/dto/add-deal.dto.ts
@@ -1,32 +1,120 @@
 import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
-import { IsNotEmpty } from "class-validator";
+import { Type } from "class-transformer";
+import { IsNotEmpty, ValidateIf, ValidateNested } from "class-validator";
+import { ModulesName } from "src/enum/module.enum";
+import { HotelLocationDeal } from "./update-deal.dto";
 
 export class AddDealDto {
+           @IsNotEmpty({
+               message: `Please enter module id&&&subject&&&Please enter module id.`,
+           })
+           @ApiProperty({
+               description: "Enter module id",
+               example: "",
+           })
+           module_id: number;
 
-    @IsNotEmpty({
-        message: `Please enter module id&&&subject&&&Please enter module id.`
-    })
-    @ApiProperty({
-        description: 'Enter module id',
-        example: ''
-    })
-    module_id: number;
+           @ApiPropertyOptional({
+               type: "string",
+               format: "binary",
+               description: "deal image (Allow Only 'JPG,JPEG,PNG')",
+               example: "",
+           })
+           image: string;
 
-    @ApiPropertyOptional({
-        type: "string",
-        format: "binary",
-        description: "deal image (Allow Only 'JPG,JPEG,PNG')",
-        example: "",
-    })
-    image: string;
+           @ValidateIf((o) => o.module_id == ModulesName.FLIGHT)
+           @IsNotEmpty({
+               message: `Please enter location &&&subject&&&Please enter location.`,
+           })
+           @ApiProperty({
+               description: "Enter location",
+               example: "",
+           })
+           location: string;
 
-    @IsNotEmpty({
-        message: `Please enter location &&&subject&&&Please enter location.`
-    })
-    @ApiProperty({
-        description: 'Enter location',
-        example: ''
-    })
-    location: string;
+           @ValidateIf((o) => o.module_id == ModulesName.HOTEL)
+           @ValidateNested({ each: true })
+           @Type(() => HotelLocationDeal)
+           @ApiProperty({
+               description: `hotel_location`,
+               example: `{
+            title: "TD Waterhouse Stadium, London, Ontario, Canada",
+            city: "London",
+            state: "Ontario",
+            country: "Canada",
+            type: "poi",
+            hotel_id: "",
+            lat: "42.9998",
+            long: "-81.2734",
+        }`,
+           })
+           hotel_location: HotelLocationDeal;
+       }
+// export class HotelLocationDeal {
+//     @IsNotEmpty({
+//         message: `Please select title.`,
+//     })
+//     @ApiProperty({
+//         description: `title`,
+//         example: ``,
+//     })
+//     title: string;
 
-}
+//     @IsNotEmpty({
+//         message: `Please select city.`,
+//     })
+//     @ApiProperty({
+//         description: `city`,
+//         example: ``,
+//     })
+//     city: string;
+
+//     @IsNotEmpty({
+//         message: `Please select state.`,
+//     })
+//     @ApiProperty({
+//         description: `state`,
+//         example: ``,
+//     })
+//     state: string;
+
+//     @IsNotEmpty({
+//         message: `Please select country.`,
+//     })
+//     @ApiProperty({
+//         description: `country`,
+//         example: ``,
+//     })
+//     country: string;
+
+//     @IsNotEmpty({
+//         message: `Please select type.`,
+//     })
+//     @ApiProperty({
+//         description: `type`,
+//         example: ``,
+//     })
+//     type: string;
+//     @IsNotEmpty({
+//         message: `Please select lat.`,
+//     })
+//     @ApiProperty({
+//         description: `lat`,
+//         example: ``,
+//     })
+//     lat: string;
+//     @IsNotEmpty({
+//         message: `Please select long.`,
+//     })
+//     @ApiProperty({
+//         description: `long`,
+//         example: ``,
+//     })
+//     long: string;
+
+//     @ApiPropertyOptional({
+//         description: "hotel_id",
+//         example: "",
+//     })
+//     hotel_id: string;
+// }
