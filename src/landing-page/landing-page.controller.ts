5d5408921f809c07db57453df741f001569cd08c
Suresh Suthar <suresh@itoneclick.com>
2021-05-24 13:13:33 +0530

    changes

--- src/landing-page/landing-page.controller.ts
+++ src/landing-page/landing-page.controller.ts
@@ -20,6 +20,7 @@ import { User } from "src/entity/user.entity";
 import { Role } from "src/enum/role.enum";
 import { Roles } from "src/guards/role.decorator";
 import { RolesGuard } from "src/guards/role.guard";
+import { ExportReferralDto } from "./dto/export-referrals.dto";
 import { ListLandingPageDto } from "./dto/list-landing-pages.dto";
 import { ListReferralDto } from "./dto/list-refferals.dto";
 import { CreateLandingPageDto } from "./dto/new-landing-page.dto";
@@ -162,6 +163,59 @@ export class LandingPageController {
     @HttpCode(200)
     async getlandingPageBookings(@Query() paginationOption: ListReferralDto) {
         return await this.landingPageService.listReferralBooking(
+            paginationOption
+        );
+    }
+
+    @Get(`export/referral-users`)
+    @ApiBearerAuth()
+    @UseGuards(AuthGuard(), RolesGuard)
+    @Roles(Role.SUPER_ADMIN, Role.SUPPORT, Role.ADMIN)
+    @ApiOperation({
+        summary: "Get detail refferal users of landing page from admin panel",
+    })
+    @ApiResponse({ status: 200, description: "Api success" })
+    @ApiResponse({
+        status: 422,
+        description: "Bad Request or API error message",
+    })
+    @ApiResponse({
+        status: 403,
+        description: "You are not allowed to access this resource.",
+    })
+    @ApiResponse({ status: 404, description: "Admin not found!" })
+    @ApiResponse({ status: 500, description: "Internal server error!" })
+    @HttpCode(200)
+    async exportlandingPageusers(@Query() paginationOption: ExportReferralDto) {
+        return await this.landingPageService.exportReferralUser(
+            paginationOption
+        );
+    }
+
+    @Get(`export/referral-bookings`)
+    @ApiBearerAuth()
+    @UseGuards(AuthGuard(), RolesGuard)
+    @Roles(Role.SUPER_ADMIN, Role.SUPPORT, Role.ADMIN)
+    @ApiOperation({
+        summary:
+            "Get detail refferal bookings of landing page from admin panel",
+    })
+    @ApiResponse({ status: 200, description: "Api success" })
+    @ApiResponse({
+        status: 422,
+        description: "Bad Request or API error message",
+    })
+    @ApiResponse({
+        status: 403,
+        description: "You are not allowed to access this resource.",
+    })
+    @ApiResponse({ status: 404, description: "Admin not found!" })
+    @ApiResponse({ status: 500, description: "Internal server error!" })
+    @HttpCode(200)
+    async exportlandingPageBookings(
+        @Query() paginationOption: ExportReferralDto
+    ) {
+        return await this.landingPageService.exportReferralBooking(
             paginationOption
         );
     }
