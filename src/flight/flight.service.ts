cd37eb50e657bf5bfdeca3f5805242c0cfe0f146
Suresh Suthar <suresh@itoneclick.com>
2021-05-31 20:50:47 +0530

    changes

--- src/flight/flight.service.ts
+++ src/flight/flight.service.ts
@@ -1171,7 +1171,7 @@ export class FlightService {
 
         //var count = await this.getDifferenceInDays(startDate, endDate);
 
-        var count = 7
+        var count = 6
         const mystiflyConfig = await new Promise((resolve) =>
             resolve(mystifly.getMystiflyCredential())
         );
