f553c45fa2f6751ea47a50c3ed1629b036b1a68e
Parth virani <parthvirani@itoneclick.com>
2021-06-22 15:52:42 +0530

    changes added

--- src/utility/landing-page.utility.ts
+++ src/utility/landing-page.utility.ts
@@ -4,122 +4,122 @@ import { LANDING_PAGE } from 'src/config/landing-page.config';
 import { LandingPages } from 'src/entity/landing-page.entity';
 import { getConnection } from 'typeorm';
 export class LandingPage {
-    static async getLandingPageValidity(lpNumber){
+	static async getLandingPageValidity(lpNumber) {
 
-      let where = `"landingPages"."is_deleted" = false AND "landingPages"."status" = true AND "landingPages"."name" like '${lpNumber}'`;
+		let where = `"landingPages"."is_deleted" = false AND "landingPages"."status" = true AND "landingPages"."name" like '${lpNumber}'`;
 
-      const query = getConnection()
-        .createQueryBuilder(LandingPages, "landingPages")
-        // .leftJoinAndSelect("landingPages.createByUser", "Users")
-        // .select([
-        //     "landingPages",
-        //     "Users.firstName",
-        //     "Users.lastName",
-        //     "Users.email",
-        // ])
-        .where(where);
+		const query = getConnection()
+			.createQueryBuilder(LandingPages, "landingPages")
+			// .leftJoinAndSelect("landingPages.createByUser", "Users")
+			// .select([
+			//     "landingPages",
+			//     "Users.firstName",
+			//     "Users.lastName",
+			//     "Users.email",
+			// ])
+			.where(where);
 
-      const landingPageDetail = await query.getOne();
-    
-      if (landingPageDetail && LANDING_PAGE[landingPageDetail?.name].applicable){
-          return true;
-        }
-        return false;
-      }
-      
-    static async getOfferData(lpNumber,type,searchData){
-      
-      if (await this.getLandingPageValidity(lpNumber)){
-          
-          switch(type){
-              case 'flight' : 
-                  return this.checkFlightoffer(lpNumber,searchData)
-              case 'hotel' :
-                  return this.checkHotelOffer(lpNumber,searchData);
-              default :
-                  return {applicable : false}
-            }
-        }
-        else{
-          return {applicable : false}
-        }
+		const landingPageDetail = await query.getOne();
 
-    }
+		if (landingPageDetail && LANDING_PAGE[landingPageDetail?.name].applicable) {
+			return true;
+		}
+		return false;
+	}
 
-    static checkFlightoffer(lpNumber,searchData){
-        let LANDING_PAGE_DATA =LANDING_PAGE[lpNumber];
-        let isRouteExist = LANDING_PAGE_DATA.deals.flight.findIndex(deal=>{
-            return  deal.from.code==searchData.departure && deal.to.code==searchData.arrival; 
-          })
-          if(isRouteExist==-1){
-            return {applicable : false}
-          }
-          
-          if(moment(searchData.checkInDate).diff(moment(),'days')<LANDING_PAGE_DATA.promotional.min_promotional_day){
-            return {applicable : false}
-          }
-    
-          return {
-            applicable : true,
-            payment_frequency_options : LANDING_PAGE_DATA.payment_frequency_options,
-            down_payment_options : LANDING_PAGE_DATA.down_payment_options,
-            discount : LANDING_PAGE_DATA.discount
-          }
-    }
+	static getOfferData(lpNumber, type, searchData) {
 
-    static checkHotelOffer(lpNumber,searchData){
+		if (LANDING_PAGE[lpNumber].applicable) {
 
-        let LANDING_PAGE_DATA =LANDING_PAGE[lpNumber];
-        let isRouteExist = LANDING_PAGE_DATA.deals.hotel.findIndex(deal=>{
-            return  deal.location.city==searchData.departure
-        })
-        if(isRouteExist==-1){
-          return {applicable : false};
-        }
+			switch (type) {
+				case 'flight':
+					return this.checkFlightoffer(lpNumber, searchData)
+				case 'hotel':
+					return this.checkHotelOffer(lpNumber, searchData);
+				default:
+					return { applicable: false }
+			}
+		}
+		else {
+			return { applicable: false }
+		}
 
-        if(moment(searchData.checkInDate).diff(moment(),'days')<LANDING_PAGE_DATA.promotional.min_promotional_day){
-          return {applicable : false};
-        }
+	}
 
-        return {
-          applicable : true,
-          payment_frequency_options : LANDING_PAGE_DATA.payment_frequency_options,
-          down_payment_options : LANDING_PAGE_DATA.down_payment_options,
-          discount : LANDING_PAGE_DATA.discount
-        }
-    }
+	static checkFlightoffer(lpNumber, searchData) {
+		let LANDING_PAGE_DATA = LANDING_PAGE[lpNumber];
+		let isRouteExist = LANDING_PAGE_DATA.deals.flight.findIndex(deal => {
+			return deal.from.code == searchData.departure && deal.to.code == searchData.arrival;
+		})
+		if (isRouteExist == -1) {
+			return { applicable: false }
+		}
 
-    static getDownPayment(offerData,downPaymentOption){
+		if (moment(searchData.checkInDate).diff(moment(), 'days') < LANDING_PAGE_DATA.promotional.min_promotional_day) {
+			return { applicable: false }
+		}
 
-      if(offerData.applicable){
-        return offerData.down_payment_options[downPaymentOption].amount ? offerData.down_payment_options[downPaymentOption].amount :null; 
-      }
-      return null;
-    }
+		return {
+			applicable: true,
+			payment_frequency_options: LANDING_PAGE_DATA.payment_frequency_options,
+			down_payment_options: LANDING_PAGE_DATA.down_payment_options,
+			discount: LANDING_PAGE_DATA.discount
+		}
+	}
 
-    static applyDiscount(offerData,price){
+	static checkHotelOffer(lpNumber, searchData) {
 
-      if(!offerData.applicable){
+		let LANDING_PAGE_DATA = LANDING_PAGE[lpNumber];
+		let isRouteExist = LANDING_PAGE_DATA.deals.hotel.findIndex(deal => {
+			return deal.location.city == searchData.departure
+		})
+		if (isRouteExist == -1) {
+			return { applicable: false };
+		}
 
-        return price;
-      }
+		if (moment(searchData.checkInDate).diff(moment(), 'days') < LANDING_PAGE_DATA.promotional.min_promotional_day) {
+			return { applicable: false };
+		}
 
-      if(offerData.discount.applicable){
-        let discountPrice;
+		return {
+			applicable: true,
+			payment_frequency_options: LANDING_PAGE_DATA.payment_frequency_options,
+			down_payment_options: LANDING_PAGE_DATA.down_payment_options,
+			discount: LANDING_PAGE_DATA.discount
+		}
+	}
 
-        if(offerData.discount.type=='flat'){
-          discountPrice = price-offerData.discount.amount;
-        }
-        else{
-          discountPrice = price-(price*offerData.discount.amount/100);  
-        }
+	static getDownPayment(offerData, downPaymentOption) {
 
-        if(discountPrice<0){
-          return 0;
-        }
-        return discountPrice;
-      }
-      return price;
-    }
+		if (offerData.applicable) {
+			return offerData.down_payment_options[downPaymentOption].amount ? offerData.down_payment_options[downPaymentOption].amount : null;
+		}
+		return null;
+	}
+
+	static applyDiscount(offerData, price) {
+
+		if (!offerData.applicable) {
+
+			return price;
+		}
+
+		if (offerData.discount.applicable) {
+			let discountPrice;
+
+			if (offerData.discount.type == 'flat') {
+				discountPrice = price - offerData.discount.amount;
+			}
+			else {
+				discountPrice = price - (price * offerData.discount.amount / 100);
+			}
+
+			if (discountPrice < 0) {
+				return 0;
+			}
+			return discountPrice;
+		}
+		return price;
+	}
 }
 
