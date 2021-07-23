80d5bd73ee9a9e856cabdfc811cd14f1daa34331
Parth virani <parthvirani@itoneclick.com>
2021-07-23 11:30:20 +0530

    changes of jaymeesh airport entity

--- src/entity/airport.entity.ts
+++ src/entity/airport.entity.ts
@@ -38,6 +38,18 @@ export class Airport extends BaseEntity {
   @Column("boolean", { name: "is_deleted", default: () => "false" })
   isDeleted: boolean;
 
-  @Column("integer", { name: "parent_id",default: () => 0 })
+  @Column("integer", { name: "parent_id", default: () => 0 })
   parentId: number;
+
+  @Column("boolean", { name: "is_blacklisted", default: () => "false" })
+  isBlackListed: boolean;
+
+  @Column("uuid", { name: "update_by", nullable: true })
+  updateBy: string;
+
+  @Column("timestamp without time zone", {
+    name: "update_date",
+    nullable: true,
+  })
+  updateDate: Date;
 }
