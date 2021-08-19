c717748cf161d8bba520e21b5ca6dc475b517dde
Jaymees.Jaymees_Donga <Jaymees.jaymees@itoneclick.com>
2021-08-19 10:21:32 +0530

    changes

--- src/landing-page/dto/new-landing-page.dto.ts
+++ src/landing-page/dto/new-landing-page.dto.ts
@@ -44,44 +44,44 @@ export class CreateLandingPageDto {
   default_setting_applied: boolean
 
 
-  @ValidateNested({ each: true })
-  @Type(() => NewLandingPageDownPaymentConfigDto)
-  @ApiProperty({
-    description: `landing_page_configuration`,
-    example: {
-      "module_id": [1, 3],
-      "days_config_id": 2,
-      "landing_page_id": "53559f02-3394-4dd8-9488-5684b6f10eb9",
-      "offer_criteria": [
-        {
-          "flight": {
-            "offer_criteria_type": "arrival",
-            "offer_criteria_variable": "airport_code",
-            "offer_criteria_value": [
-              "AMD"
-            ]
-          },
-          "hotel": {
-            "offer_criteria_type": "city",
-            "offer_criteria_variable": "city",
-            "offer_criteria_value": [
-              "Ahmedabad"
-            ]
-          }
-        }
-      ],
-      "down_payment_option": [
-        20,
-        30,
-        40
-      ],
-      "payment_frequency": [
-        "monthly",
-        "weekly",
-        "biweekly"
-      ],
-      "down_payment_type": "percentage"
-    }
-  })
-  config: NewLandingPageDownPaymentConfigDto
+  // @ValidateNested({ each: true })
+  // @Type(() => NewLandingPageDownPaymentConfigDto)
+  // @ApiProperty({
+  //   description: `landing_page_configuration`,
+  //   example: {
+  //     "module_id": [1, 3],
+  //     "days_config_id": 2,
+  //     "landing_page_id": "53559f02-3394-4dd8-9488-5684b6f10eb9",
+  //     "offer_criteria": [
+  //       {
+  //         "flight": {
+  //           "offer_criteria_type": "arrival",
+  //           "offer_criteria_variable": "airport_code",
+  //           "offer_criteria_value": [
+  //             "AMD"
+  //           ]
+  //         },
+  //         "hotel": {
+  //           "offer_criteria_type": "city",
+  //           "offer_criteria_variable": "city",
+  //           "offer_criteria_value": [
+  //             "Ahmedabad"
+  //           ]
+  //         }
+  //       }
+  //     ],
+  //     "down_payment_option": [
+  //       20,
+  //       30,
+  //       40
+  //     ],
+  //     "payment_frequency": [
+  //       "monthly",
+  //       "weekly",
+  //       "biweekly"
+  //     ],
+  //     "down_payment_type": "percentage"
+  //   }
+  // })
+  // config: NewLandingPageDownPaymentConfigDto
 }
